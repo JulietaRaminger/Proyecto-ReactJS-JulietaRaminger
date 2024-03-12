50ba4ec21448f4f11c3e0b9646a6f3b570ce6326
Julieta Raminger <julieta.raminger@gmail.com>
2024-03-12 00:36:57 -0300

    Primera pre-entrega ReactJS

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import './App.css';
+import { ItemListContainer } from './components/ItemListContainer/ItemListContainer';
+import NavBar from './components/NavBar/NavBar';
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <ItemListContainer greeting="Bienvenidos" />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/CartWidget/CartWidget.jsx
@@ -0,0 +1,12 @@
+import cart from '../../assets/cart.png';
+
+export const CartWidget = () => {
+  return (
+    <div id="cart-widget">
+      <img src={cart} alt="Cart" className="img" />
+      <span>0</span>
+    </div>
+  );
+};
+
+export default CartWidget;
