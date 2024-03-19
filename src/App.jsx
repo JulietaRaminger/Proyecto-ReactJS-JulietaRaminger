dbc084c0d3aa4884c8491b993f496a9b6201a502
Julieta Raminger <julieta.raminger@gmail.com>
2024-03-19 20:02:10 -0300

    Proyecto final React JS

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { ItemListContainer } from './components/ItemListContainer/ItemListContainer';
 import NavBar from './components/NavBar/NavBar';
+import ItemCount from './components/ItemCount/ItemCount';
+
 
 function App() {
   return (
     <>
       <NavBar />
       <ItemListContainer greeting="Bienvenidos" />
+      <ItemCount
+        initial={1}
+        stock={10}
+        onAdd={(quantity) => console.log('Cantidad agregada ', quantity)}
+      />
     </>
   );
 }
